0be482839fadb6a1b3b0057195739a33c3c90ef2
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-09-01 09:47:02 +0900

    D-7
    v-2.0
    ---
    1. v-2.0 => PIC12, D-6 ==> Retry
    	==> GP0 not outputting voltage

--- D-7_PIC12_Wave/D-7_PIC12_Wave_v-2.0/D-7_PIC12_Wave_v-2.0.asm
+++ D-7_PIC12_Wave/D-7_PIC12_Wave_v-2.0/D-7_PIC12_Wave_v-2.0.asm
@@ -3,12 +3,12 @@
 	
 	__CONFIG	_CP_OFF & _CPD_OFF & _BODEN_OFF & _MCLRE_OFF & _WDT_OFF & _PWRTE_ON & _INTRC_OSC_NOCLKOUT
 	
-w_temp		EQU	0x20
-CNT1		EQU	0x21
+;w_temp		EQU	0x20
+;CNT1		EQU	0x21
 CNT2		EQU	0x22
-CNT3		EQU	0x23
-O_DATA		EQU	0x24
-TMP_DATA	EQU	0x25
+;CNT3		EQU	0x23
+;O_DATA		EQU	0x24
+;TMP_DATA	EQU	0x25
 	
 				ORG		0x000
 				goto	main
@@ -32,44 +32,22 @@ main
 		
 		; GPIO
 		bsf		STATUS,RP0
-		movlw	b'11101000'
+		movlw	b'11111100'
 		movwf	TRISIO
 		
 		bcf		STATUS,RP0
 		
-		clrf	CNT1
+		;clrf	CNT1
 		
 ;main_loop
 ;{		
 main_loop
-		; GPIO3 => Lo?
-		;btfss	GPIO,3
-		;goto	main_loop
 
-		; Read CNT1
-		movf	CNT1,w
-		movwf	TMP_DATA
-		
-		; Extract => 4th bit
-		andlw	b'00001000'
-		movwf	O_DATA
-		
-		; 4th bit => to 5th bit
-		bcf		STATUS,C
-		rlf		O_DATA,f
-		movf	TMP_DATA,w
-		
-		; The remaining first 3 bits
-		andlw	b'00000111'
-		iorwf	O_DATA,f
-		
-		; Output data
-		movf	O_DATA,w
-		movwf	GPIO
-		
+		bsf		GPIO,0
 		call	DLY_1
 		
-		incf	CNT1,f
+		bcf		GPIO,0
+		call	DLY_1
 		
 		goto	main_loop
 
@@ -79,7 +57,7 @@ main_loop
 ;DLY_1
 ;{		
 DLY_1
-	movlw		d'20'
+	movlw		d'100'
 	movwf		CNT2
 	
 DLY_1L
