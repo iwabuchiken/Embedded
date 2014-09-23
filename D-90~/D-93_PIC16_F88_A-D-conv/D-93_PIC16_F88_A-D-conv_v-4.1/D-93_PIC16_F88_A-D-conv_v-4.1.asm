740bb83296402b894ed5c3a24245c3036bd5e9cf
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-09-23 13:19:09 +0900

    D-93(m)
    v-4.1.1
    ---
    1. LED => on/off
    
             ==> W

--- D-90~/D-93_PIC16_F88_A-D-conv/D-93_PIC16_F88_A-D-conv_v-4.1/D-93_PIC16_F88_A-D-conv_v-4.1.asm
+++ D-90~/D-93_PIC16_F88_A-D-conv/D-93_PIC16_F88_A-D-conv_v-4.1/D-93_PIC16_F88_A-D-conv_v-4.1.asm
@@ -4,6 +4,14 @@
 	;__CONFIG _CONFIG1, _HS_OSC & _WDT_OFF & _PWRITE_ON & _CP_OFF & _BODEN_ON & _LVP_OFF & _CPD_OFF & _DEBUG_OFF
 	__CONFIG _CONFIG1, _HS_OSC & _WDT_OFF & _PWRTE_ON & _CP_OFF & _BODEN_ON & _LVP_OFF & _CPD_OFF & _DEBUG_OFF
 
+	CBLOCK	020h
+	
+	CNT1	
+	CNT2
+	CNT3
+	
+	ENDC
+	
 	ORG		0
 
 MAIN
@@ -16,6 +24,45 @@ LOOP
 	MOVLW	02h
 	MOVWF	PORTB
 	
+	CALL	DLY_1S
+	
+	MOVLW	01h
+	MOVWF	PORTB
+	
+	CALL	DLY_1S
+	
 	GOTO	LOOP
+
+;DLY_1S
+;{		
+DLY_1S
+	movlw		d'10'
+	movwf		CNT3
+	
+DLY_100
+	movlw		d'100'
+	movwf		CNT1
+
+DLP1
+	movlw		d'200'
+	movwf		CNT2
+	
+DLP2
+	nop
+	nop
+	
+	decfsz		CNT2,f
+	goto		DLP2
+	
+	decfsz		CNT1,f
+	goto		DLP1
+	
+	decfsz		CNT3,f
+	goto		DLY_100
+	
+	retlw		0
+;}
+;
+
 
 	END
