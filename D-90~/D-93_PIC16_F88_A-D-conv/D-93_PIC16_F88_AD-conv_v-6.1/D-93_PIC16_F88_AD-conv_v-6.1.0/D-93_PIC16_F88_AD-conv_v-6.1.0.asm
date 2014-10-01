16ab56d94e911c11810786731abd52d6dd047853
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-10-01 16:41:55 +0900

    D-93
    v-6.1.0a
    ---
    1. use "chg7seg"
    	=> use decimal numbers
    
        => w

---
+++ D-90~/D-93_PIC16_F88_A-D-conv/D-93_PIC16_F88_AD-conv_v-6.1/D-93_PIC16_F88_AD-conv_v-6.1.0/D-93_PIC16_F88_AD-conv_v-6.1.0.asm
@@ -0,0 +1,194 @@
+	LIST	P=PIC16F88
+	INCLUDE	P16F88.INC
+
+	__CONFIG _CONFIG1, _HS_OSC & _WDT_OFF & _PWRTE_OFF & _CP_OFF & _LVP_OFF & _CPD_OFF
+	__CONFIG _CONFIG2, _FCMEN_OFF & _IESO_OFF
+;
+;
+;{CBLOCK
+	CBLOCK	0Ch
+	C02mS	; 0.2mSカウンタ
+	C5mS	; 5mSカウンタ
+	C1S	; 1Sカウンタ
+	ENDC	
+;}
+;
+	ORG		0
+	GOTO	INIT
+
+;	ORG		4
+;	CALL	Timer0_interrupt
+;	RETFIE
+
+; ====================================== INIT
+;INIT
+;{
+INIT
+
+	;------------------ switch bank
+	BSF		STATUS,RP0
+	
+	;------------------ ANSEL
+	CLRF	ANSEL
+
+	;------------------ TRISB, TRISB
+	CLRF	TRISB		; output
+	CLRF	TRISA		; output
+
+	;------------------ RB0 => ON, RA => OFF
+	;MOVLW	01h
+	MOVLW	00h
+	MOVWF	PORTB
+
+	MOVLW	00h
+	MOVWF	PORTA
+	
+;	;------------------ OPTION_REG
+;	MOVLW	88h
+;	MOVWF	OPTION_REG
+;	
+	;------------------ switch bank
+	BCF		STATUS,RP0
+	
+;	;------------------ interrupt
+;	CLRF	TMR0
+;
+;	BSF		INTCON,TMR0IE	; permit TMR0 interrupt
+;							; |= 0x20
+;							
+;	BSF		INTCON,GIE		; all interrupt => permitted
+;							;	--> except masked ones
+;							; |= 0x80
+;	
+;	;BCF		INTCON,TMR0IF	; clear the interrupt flag of TMR0
+	
+
+;}
+;
+; ====================================== STEP1
+;{
+STEP1	
+	
+	;---------------- digit 1
+	MOVLW	D'1'
+	MOVWF	PORTA
+	
+	CALL	bin2hex
+	MOVWF	PORTB
+	
+	CALL	T5mS
+	
+	;---------------- digit 2
+	MOVLW	D'2'
+	MOVWF	PORTA
+	
+	CALL	bin2hex
+	MOVWF	PORTB
+	
+	CALL	T5mS
+	
+	;---------------- digit 4
+	MOVLW	D'4'
+	MOVWF	PORTA
+	
+	CALL	bin2hex
+	MOVWF	PORTB
+	
+	CALL	T5mS
+
+	
+	GOTO	STEP1
+;}
+;
+
+; ====================================== Timer0_interrupt
+;{
+Timer0_interrupt
+
+	BCF		INTCON,GIE		; forbid global interrupt
+	BCF		INTCON,TMR0IE	; forbid timer interrupt
+	
+	BCF		INTCON,TMR0IF	; clear flag
+
+	;------------------- change LED
+	
+	MOVLW	B'00000001'
+	MOVWF	PORTA
+	MOVWF	PORTB
+	
+	MOVLW	B'00000010'
+	MOVWF	PORTA
+	MOVWF	PORTB
+	
+	;------------------- re-init TMR0
+	MOVLW	B'11110000'
+	MOVWF	TMR0
+	
+	;------------------- reset
+	BSF		INTCON,TMR0IE	; permit timer interrupt
+	BSF		INTCON,GIE		; permit global interrupt
+	
+
+	RETURN
+
+;}
+;
+; ====================================== T1S
+;{    ; ＜＜　1S待つサブルーチン　＞＞   
+T1S
+	MOVLW	d'200'
+	MOVWF	C1S		; ループ回数として200をセット）
+T1SLP	CALL	T5mS		; 5mS待つ
+	DECFSZ	C1S,F		; ループ回数－１して０になったら次の命令をスキップ
+	GOTO	T1SLP		;
+	RETURN			; このサブルーチン呼出し元に戻る
+
+
+    ; ＜＜　5mS待つサブルーチン　＞＞   
+T5mS
+	MOVLW	d'25'
+	MOVWF	C5mS		; ループ回数として25をセット）
+T5mLP	CALL	T02mS		; 0.2mS待つ
+	DECFSZ	C5mS,F		; ループ回数－１して０になったら次の命令をスキップ
+	GOTO	T5mLP
+	RETURN			; このサブルーチン呼出し元に戻る
+
+
+    ; ＜＜　0.2mS待つサブルーチン　＞＞   
+T02mS
+	MOVLW	d'249'
+	MOVWF	C02mS		; ループ回数として249をセット
+T02mLP	NOP			; 何もせず１サイクル消費
+	DECFSZ	C02mS,F		; ループ回数－１して０になったら次の命令をスキップ
+	GOTO	T02mLP
+	RETURN			; このサブルーチン呼出し元に戻る
+;}
+;
+
+; -----------------------------------------------------
+;{ バイナリ４ビットを７セグメントＬＥＤ表示用１６進数に変換
+bin2hex
+	ADDWF	PCL,f
+
+	RETLW	0FCh	;0
+	RETLW	060h	;1
+	RETLW	0DAh	;2
+	RETLW	0F2h	;3
+	
+	RETLW	066h	;4
+	RETLW	06Dh	;5
+	RETLW	07Dh	;6
+	RETLW	007h	;7
+	RETLW	07Fh	;8
+	RETLW	06Fh	;9
+	RETLW	077h	;A
+	RETLW	07Ch	;b
+	RETLW	039h	;C
+	RETLW	05Eh	;d
+	RETLW	079h	;E
+	RETLW	071h	;F
+;}
+;
+
+;========================================
+	END
