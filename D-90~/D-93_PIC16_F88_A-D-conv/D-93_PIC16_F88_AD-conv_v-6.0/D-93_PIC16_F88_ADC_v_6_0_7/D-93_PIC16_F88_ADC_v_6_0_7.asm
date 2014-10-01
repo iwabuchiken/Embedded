67fee1026f0eceafd32e777caa10db4c3473a1ad
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-10-01 15:11:05 +0900

    D-93
    v-6.0.7a
    ---
    1. re-try: mplab, assembly
    
        => w

---
+++ D-90~/D-93_PIC16_F88_A-D-conv/D-93_PIC16_F88_AD-conv_v-6.0/D-93_PIC16_F88_ADC_v_6_0_7/D-93_PIC16_F88_ADC_v_6_0_7.asm
@@ -0,0 +1,96 @@
+	LIST	P=PIC16F88
+	INCLUDE	P16F88.INC
+
+	__CONFIG _CONFIG1, _HS_OSC & _WDT_OFF & _PWRTE_OFF & _CP_OFF & _LVP_OFF & _CPD_OFF
+	__CONFIG _CONFIG2, _FCMEN_OFF & _IESO_OFF
+
+	ORG		0
+	GOTO	INIT
+
+	ORG		4
+	CALL	Timer0_interrupt
+	RETFIE
+
+; ====================================== INIT
+;INIT
+;{
+INIT
+
+	;------------------ switch bank
+	BSF		STATUS,RP0
+	
+	;------------------ ANSEL
+	CLRF	ANSEL
+
+	;------------------ TRISB, TRISB
+	CLRF	TRISB		; output
+	CLRF	TRISA		; output
+
+	;------------------ RB0 => ON, RA => OFF
+	MOVLW	01h
+	MOVWF	PORTB
+
+	MOVLW	00h
+	MOVWF	PORTA
+	
+	;------------------ OPTION_REG
+	MOVLW	88h
+	MOVWF	OPTION_REG
+	;MOVLW	88h
+	;MOVWF	OPTION_REG
+	
+	;------------------ switch bank
+	BCF		STATUS,RP0
+	
+	;------------------ interrupt
+	CLRF	TMR0
+
+	BSF		INTCON,TMR0IE	; permit TMR0 interrupt
+							; |= 0x20
+							
+	BSF		INTCON,GIE		; all interrupt => permitted
+							;	--> except masked ones
+							; |= 0x80
+	
+	;BCF		INTCON,TMR0IF	; clear the interrupt flag of TMR0
+	
+
+;}
+;
+; ====================================== STEP1
+;{
+STEP1	
+
+	;MOVLW	B'00000000'
+	;MOVWF	PORTB
+	
+	GOTO	STEP1
+;}
+;
+
+; ====================================== Timer0_interrupt
+;{
+Timer0_interrupt
+
+	BCF		INTCON,GIE		; forbid global interrupt
+	BCF		INTCON,TMR0IE	; forbid timer interrupt
+	
+	BCF		INTCON,TMR0IF	; clear flag
+
+	MOVLW	B'00000001'
+	MOVWF	PORTA
+	
+	MOVLW	B'00000010'
+	MOVWF	PORTA
+	
+	;------------------- reset
+	BSF		INTCON,TMR0IE	; permit timer interrupt
+	BSF		INTCON,GIE		; permit global interrupt
+	
+
+	RETURN
+
+;}
+;
+
+	END
