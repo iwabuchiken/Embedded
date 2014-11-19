4590a78fabbae78b2e48db3744957774af67a78d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-11-19 15:08:04 +0900

    D-108
    v-2.3.0
    ---
    1. LCD => again, with MPLAB X
    
        ==> w

---
+++ D-100~/D-108_Aoki_TMR0/D-108_v_2_3/D-108_v_2_3.X/D-108_v_2_3.c
@@ -0,0 +1,63 @@
+/*
+ * File:   D-108_v_2_3.c
+ * Author: iwabuchik
+ *
+ * Created on 2014/11/19, 14:43
+ */
+
+
+#include <xc.h>
+
+#include "SD1602_4bit_mode.h"
+
+#ifndef _XTAL_FREQ
+#define _XTAL_FREQ 20000000
+#endif
+
+#pragma config FOSC = HS
+#pragma config WDTE = OFF
+#pragma config PWRTE = ON
+#pragma config CP = OFF
+
+void _Setup(void);
+
+void main(void) {
+
+	_Setup();
+
+	while(1) {
+
+		PORTA = 0x01;
+
+		__delay_ms(500);
+
+		PORTA = 0x00;
+
+		__delay_ms(500);
+
+	}
+
+    return;
+
+}
+
+void _Setup(void) {
+
+    /****************
+     * TRISB
+     ****************/
+    TRISB = 0x00;
+    TRISA = 0x00;
+
+    /****************
+     * PORT
+     ****************/
+    PORTA = 0x00;
+    PORTB = 0x00;
+
+    /****************
+     * ANSEL
+     ****************/
+    ANSEL = 0x00;
+
+}//_Setup
