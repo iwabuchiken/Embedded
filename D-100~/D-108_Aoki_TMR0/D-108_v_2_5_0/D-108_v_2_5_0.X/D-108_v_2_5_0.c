b304b178b2445cbb01c34ac42bafbdb534e71013
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-11-20 16:42:34 +0900

    D-108
    v-2.5.0c
    ---
    1. change messages => while button being pushed
    
        ==> w

--- D-100~/D-108_Aoki_TMR0/D-108_v_2_5_0/D-108_v_2_5_0.X/D-108_v_2_5_0.c
+++ D-100~/D-108_Aoki_TMR0/D-108_v_2_5_0/D-108_v_2_5_0.X/D-108_v_2_5_0.c
@@ -7,12 +7,16 @@
 
 
 #include <xc.h>
+#include <string.h>
 
 #include "SD1602_4bit_mode.h"
 
 #ifndef _XTAL_FREQ
 #define _XTAL_FREQ 20000000
 #endif
+
+#define true	1
+#define false	0
 
 #pragma config FOSC = HS
 #pragma config WDTE = OFF
@@ -23,6 +27,14 @@ void _Setup(void);
 void _Display(void);
 void _While(void);
 
+unsigned int msg_num = 0;	// message number
+						// 0 => version; 1 => greeting
+
+char s[20];
+
+const char msg_1[]  = "D-108 v-2.5.1b";
+const char msg_2[]  = "You clicked it!";
+
 void main(void) {
 
 	_Display();
@@ -30,16 +42,6 @@ void main(void) {
 	while(1) {
 
 		_While();
-
-//		if (PORTBbits.RB1 == 1) {
-//
-//			PORTBbits.RB0 = 1;
-//
-//		} else {
-//
-//			PORTBbits.RB0 = 0;
-//
-//		}
 
 	}
 
@@ -66,11 +68,19 @@ void _Setup(void) {
      ****************/
     ANSEL = 0x00;
 
+    ///////////////////////
+
+	// vars
+
+	///////////////////////
+    msg_num = 0;
 }//_Setup
 
 void _Display(void) {
 
-	char s[]  = "D-108 v-2.5.0a";
+	strcpy(s, msg_1);
+//	strcpy(s, "D-108 v-2.5.1a");
+//	char s[]  = "D-108 v-2.5.0a";
 
 	_Setup();
 
@@ -85,16 +95,57 @@ void _Display(void) {
 
 }
 
-void _While(void) {
+void
+_While(void) {
 
 	if (PORTBbits.RB1 == 1) {
+
+		if(msg_num != 1) {
+
+			SD1602_control(0x01);	// LCD => clear
+
+			__delay_ms(2);			// "Execute time(max.)" => 1.64 ms
+
+			SD1602_control(0x02);	// Cursor => at home
+									// Exec time => 1.64 ms
+
+			__delay_ms(2);
+
+			strcpy(s, msg_2);
+
+			SD1602_print(s);
+
+			// set message number
+			msg_num = 1;
+
+		}
 
 		PORTBbits.RB0 = 1;
 
 	} else {
 
+		if(msg_num != 0) {
+
+			SD1602_control(0x01);	// LCD => clear
+
+			__delay_ms(2);			// "Execute time(max.)" => 1.64 ms
+
+			SD1602_control(0x02);	// Cursor => at home
+									// Exec time => 1.64 ms
+
+			__delay_ms(2);
+
+			strcpy(s, msg_1);
+
+			SD1602_print(s);
+
+			// set message number
+			msg_num = 0;
+
+		}
+
 		PORTBbits.RB0 = 0;
 
-	}
+	}//if (PORTBbits.RB1 == 1)
 
-}
+}//_While
