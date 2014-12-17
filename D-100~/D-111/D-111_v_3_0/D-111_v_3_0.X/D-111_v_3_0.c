bf6c09e22b3b36d4fae0aa37040f991e712150c8
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-12-17 16:36:49 +0900

    D-111
    v-3.0-5
    ---
    1. pragma directives (D-111.c)
    	==> move to main.h
    
    	==> w

--- D-100~/D-111/D-111_v_3_0/D-111_v_3_0.X/D-111_v_3_0.c
+++ D-100~/D-111/D-111_v_3_0/D-111_v_3_0.X/D-111_v_3_0.c
@@ -4,6 +4,9 @@
  *
  * Created on 10/12/2014 20:36:38
  */
+#ifndef MAIN_H
+#include "main.h"
+#endif
 
 
 #include <xc.h>
@@ -24,6 +27,7 @@
 #include "lib_v_1.h"
 #endif
 
+
 /*
  * lib_D-108_V_2_5_3.h
  *
@@ -35,28 +39,28 @@
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 
-// CONFIG1
-#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
-#pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
-#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
-//#pragma config BODEN = ON       // Brown-out Reset Enable bit (BOR enabled)
-#pragma config LVP = OFF         // Low-Voltage Programming Enable bit (RB3/PGM pin has PGM function, Low-Voltage Programming enabled)
-#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Code protection off)
-#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off)
-//#pragma config CCPMX = RB0      // CCP1 Pin Selection bit (CCP1 function on RB0)
-#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
-#pragma config DEBUG = OFF         // Flash Program Memory Code Protection bit (Code protection off)
-
-//// CONFIG2
-//#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
-//#pragma config IESO = ON        // Internal External Switchover bit (Internal External Switchover mode enabled)
-
-//#pragma config FOSC = HS
-//#pragma config WDTE = OFF
-//#pragma config PWRTE = ON
-//#pragma config CP = OFF
+//// CONFIG1
+//#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+//#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+//#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+//#pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+//#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+////#pragma config BODEN = ON       // Brown-out Reset Enable bit (BOR enabled)
+//#pragma config LVP = OFF         // Low-Voltage Programming Enable bit (RB3/PGM pin has PGM function, Low-Voltage Programming enabled)
+//#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Code protection off)
+//#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off)
+////#pragma config CCPMX = RB0      // CCP1 Pin Selection bit (CCP1 function on RB0)
+//#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+//#pragma config DEBUG = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+//
+////// CONFIG2
+////#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
+////#pragma config IESO = ON        // Internal External Switchover bit (Internal External Switchover mode enabled)
+//
+////#pragma config FOSC = HS
+////#pragma config WDTE = OFF
+////#pragma config PWRTE = ON
+////#pragma config CP = OFF
 
 ///////////////////////
 
--- D-100~/D-111/D-111_v_3_0/D-111_v_3_0.X/main.h
+++ D-100~/D-111/D-111_v_3_0/D-111_v_3_0.X/main.h
@@ -6,32 +6,32 @@
 #define _XTAL_FREQ 20000000
 #endif
 
-//// CONFIG1
-//#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
-//#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
-//#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
-//#pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
-//#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
-////#pragma config BODEN = ON       // Brown-out Reset Enable bit (BOR enabled)
-//#pragma config LVP = OFF         // Low-Voltage Programming Enable bit (RB3/PGM pin has PGM function, Low-Voltage Programming enabled)
-//#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Code protection off)
-//#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off)
-////#pragma config CCPMX = RB0      // CCP1 Pin Selection bit (CCP1 function on RB0)
-//#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
-//#pragma config DEBUG = OFF         // Flash Program Memory Code Protection bit (Code protection off)
-//
-////// CONFIG2
-////#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
-////#pragma config IESO = ON        // Internal External Switchover bit (Internal External Switchover mode enabled)
-//
-////#pragma config FOSC = HS
-////#pragma config WDTE = OFF
-////#pragma config PWRTE = ON
-////#pragma config CP = OFF
+// CONFIG1
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+//#pragma config BODEN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF         // Low-Voltage Programming Enable bit (RB3/PGM pin has PGM function, Low-Voltage Programming enabled)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off)
+//#pragma config CCPMX = RB0      // CCP1 Pin Selection bit (CCP1 function on RB0)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+#pragma config DEBUG = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+//// CONFIG2
+//#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
+//#pragma config IESO = ON        // Internal External Switchover bit (Internal External Switchover mode enabled)
+
+//#pragma config FOSC = HS
+//#pragma config WDTE = OFF
+//#pragma config PWRTE = ON
+//#pragma config CP = OFF
 
 ///////////////////////
 
 // vars
 
 ///////////////////////
-char msg_Project_Name[]  = "D-111 v-3.0-4";
+char msg_Project_Name[]  = "D-111 v-3.0-5";
