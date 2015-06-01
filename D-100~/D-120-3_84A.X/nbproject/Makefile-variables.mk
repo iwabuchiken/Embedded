0dd93722d31e09502de0bee5d79f3242a76f6f23
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-06-01 09:06:54 +0900

    D-120
    v-1.0
    ---
    1. 2 LEDs => on/off
    
        ==> done

---
+++ D-100~/D-120-3_84A.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=D-120-3_84A.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/D-120-3_84A.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=d-120-384a.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/d-120-384a.x.tar
--- D-100~/D-120.X/D-120_v_1_0.c
+++ D-100~/D-120.X/D-120_v_1_0.c
@@ -2,7 +2,7 @@
 * File:   D-120_v_1_0.c
  * Author: iwabuchik
  *
- * Created on 05/02/2015 09:11:31
+ * Created on
  */
 
 #ifndef XC_H
@@ -10,49 +10,30 @@
 #include <xc.h>
 #endif
 
-#ifndef STDIO_H
-#define STDIO_H
 #include <stdio.h>
-#endif
+#include <stdlib.h>
 
+#include "pic.h"
 
-
-#ifndef _XTAL_FREQ
 #define _XTAL_FREQ 20000000
-#endif
 
-#ifndef MAIN_H
-#include "main.h"
-#endif
+#pragma config FOSC = HS
+#pragma config WDTE = OFF
+#pragma config PWRTE = ON
+#pragma config CP = OFF
 
-#ifndef LIB__H
-#include "lib.h"
-#endif
 
 ///////////////////////
 
 // protos
 
 ///////////////////////
-void _Setup(void);
-void _Setup_ANSEL(void);
-
-void _While(void);
-
-void _Setup_Vars(void);
-
-void _Setup_PORTB(void);
 
 ///////////////////////
 
 // vars
 
 ///////////////////////
-usi count;
-
-int f_INT = false;
-
-usi cycle = 0;	// count: num of display update
 
 ///////////////////////
 
@@ -66,25 +47,9 @@ void main(void) {
 	// setups
 
 	///////////////////////
-	_Setup();			// init MCU
+	TRISB = 0x00;	// RB3:output
 
-	_Setup_ANSEL();
-
-	///////////////////////////////
-	//
-	// setup: PORTB
-	//
-	 ////////////-///////////////////
-	///////////////////////////////
-	//
-	// initial status
-	//
-	 ///////////////////////////////
-	PORTBbits.RB2 = 0;
-	PORTBbits.RB3 = 1;
-
-//	_Setup_PORTB();
-
+	PORTB = 0x00;
 
 	///////////////////////////////
 	//
@@ -93,87 +58,19 @@ void main(void) {
 	 ///////////////////////////////
 	while(1) {
 
-//		_While();
+            //REF binary http://www.microchip.com/forums/m504764.aspx
+            PORTB = 0b00001111;
+//            PORTB = 0b00000001;
+
+            __delay_ms(500);
+
+            PORTB = 0b00000000;
+
+            __delay_ms(500);
 
 	}//while(1)
 
 	return;
-}
-
-void _Setup_PORTB() {
-
-	///////////////////////////////
-	//
-	// initial status
-	//
-	 ///////////////////////////////
-	PORTBbits.RB2 = 0;
-	PORTBbits.RB3 = 1;
 
 }
-
-void _Setup_Vars() {
-
-	count = 0;
-
-}
-
-void
-_Setup_ANSEL(void) {
-
-	//REF http://homepage3.nifty.com/mitt/pic/pic88_3.html
-	/****************
-	 * ANSEL
-	 ****************/
-	ADCON0 = 0x99;	// 1001 1001
-					// Bit 5-3: CHS2-0
-					// Bit 2: GO/^DONE
-
-	ADCON1 = 0x80;	// 1000 0000
-					// Bit 8: ADFM => 1: right-aligned
-					// Bits 5-4: VCFG => 00: Vref+ = Vdd, Vref- = Vss
-
-	ANSEL = 0x08;	// 0000 1000
-					// AN3 => ADC
-
-}//_Setup_ANSEL
-
-void _Setup(void) {
-
-	/****************
-	 * OPTION_REG
-	 ****************/
-	OPTION_REGbits.nRBPU = 0;
-
-	/****************
-	 * TRIS
-	 ****************/
-	TRISB = 0b00000001;		// RB0 => input
-	TRISA = 0x00000000;		// RA => all output
-
-	/****************
-	 * PORT
-	 ****************/
-	PORTA = 0b00000000;		// initial => no output, no input
-	PORTB = 0b00001111;		// RB0~3 => H / RB4~7 => L
-//	PORTB = 0x00;
-
-}//_Setup
-
-void
-_While(void) {
-
-//	///////////////////////////////
-//	//
-//	// flag: f_INT
-//	//
-//	 ///////////////////////////////
-//	if (f_INT == true) {
-//
-//		PORTBbits.RB2 = tmp_1;
-//		PORTBbits.RB3 = tmp_2;
-//
-//	}//if (f_INT == true)
-
-}//_While
 
